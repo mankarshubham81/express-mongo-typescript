2c627004c287e415ee86f55549de5e623f06456c
Shubham Mankar <mankarshubham81@gmail.com>
2024-07-15 22:27:00 +0530

    added criteria for user password validation

--- src/middleware/ValidateSchema.ts
+++ src/middleware/ValidateSchema.ts
@@ -3,6 +3,8 @@ import { NextFunction, Response, Request } from 'express';
 import Logging from '../library/Logging';
 import { IAuthor } from '../models/Author';
 import { IBook } from '../models/Book';
+import { IUser } from '../models/User';
+import PasswordComplexity from 'joi-password-complexity';
 
 export const ValidateSchema = (schema: ObjectSchema) => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -14,6 +16,16 @@ export const ValidateSchema = (schema: ObjectSchema) => {
             return res.status(422).json({ error });
         }
     }
+};
+
+const passwordComplexityOptions = {
+    min: 8,
+    max: 255,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 1,
+    requirementCount: 4, // Adjusted to ensure all criteria are required
 };
 
 export const Schemas = {
@@ -28,17 +40,27 @@ export const Schemas = {
     book: {
         create: Joi.object<IBook>({
             author: Joi.string()
-                .regex(/^[0-9-fA-f]{24}$/)
+                .regex(/^[0-9a-fA-F]{24}$/)
                 .required(),
             title: Joi.string().required()
-
         }),
         update: Joi.object<IBook>({
             author: Joi.string()
-                .regex(/^[0-9-fA-f]{24}$/)
+                .regex(/^[0-9a-fA-F]{24}$/)
                 .required(),
             title: Joi.string().required()
-
+        })
+    },
+    user: {
+        create: Joi.object<IUser>({
+            name: Joi.string().min(5).max(50).required(),
+            email: Joi.string().min(5).max(255).required().email(),
+            password: PasswordComplexity(passwordComplexityOptions).required()
+        }),
+        update: Joi.object<IUser>({
+            name: Joi.string().min(5).max(50).required(),
+            email: Joi.string().min(5).max(255).required().email(),
+            password: PasswordComplexity(passwordComplexityOptions).required()
         })
     }
 }
