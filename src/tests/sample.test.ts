2cfa2f446c1b25cc3227dcb875391d8ed9cae26f
Shubham Mankar <mankarshubham81@gmail.com>
2024-07-25 16:40:56 +0530

    unorganizeed tests

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -9,5 +9,15 @@ test('should first test', () => {
 test('absoulte - it should return a positive number if inout is positive', () => {
     const result = absolute(1);
     expect(result).toBe(1);
-})
+});
+
+test('absoulte - it should return a positive number if input is negative', () => {
+    const result = absolute(-1);
+    expect(result).toBe(1);
+});
+
+test('absoulte - it should return a positive number if input is 0', () => {
+    const result = absolute(0);
+    expect(result).toBe(-0);
+});
 
