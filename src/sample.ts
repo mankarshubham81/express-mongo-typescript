63cedb342694f3a7fa8df0f57e3593e3722dbf93
Shubham Mankar <mankarshubham81@gmail.com>
2024-07-25 22:46:07 +0530

    test for obajects and exceptions

--- src/sample.ts
+++ src/sample.ts
@@ -13,3 +13,24 @@ export const greet = (name: string): string => {
 export const getCurrencies = (): string[] => {
     return ['USD', 'AUD', 'EUR'];
 };
+
+
+// Testing Obescts
+export const getProducts = (productId: number): {} => {
+    return { id: productId, price: 10, category: 'a' };
+};
+
+
+// Testing Exceptions
+interface User {
+    id: number;
+    userName: string;
+}
+export const registerUser = (userName: string): User => {
+
+    if (!userName) throw new Error("Username is required");
+
+
+    return { id: new Date().getTime(), userName: userName };
+};
+
--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -1,4 +1,5 @@
-import { absolute, greet, getCurrencies } from '../sample';
+import { any } from 'joi';
+import { absolute, greet, getCurrencies, getProducts, registerUser } from '../sample';
 
 
 describe('absolute', () => {
@@ -56,3 +57,47 @@ describe('getCurrencies', () => {
 
     });
 });
+
+
+describe('getProducts', () => {
+    it('should return the product with the given id', () => {
+        const result = getProducts(1);
+        // // Ways to test
+
+        // wrong matcher
+        // wrong way - because it 
+        // expect(result).toBe({ id: 1, price: 10 });
+
+        // Too Specific - should be avoided
+        // exact matcher 1 - i.e. it should have exactly all the key: value from original returned object
+        // expect(result).toEqual({ id: 1, price: 10 });
+
+        // Proper way - good way
+        expect(result).toHaveProperty('id', 1);
+
+        // Ideal way - better way
+        // right matcher 2 - at leats this fileds should be present
+        expect(result).toMatchObject({ id: 1, price: 10 });
+
+    });
+});
+
+
+describe('registerUser', () => {
+    it('should throw if Username is falsy', () => {
+        // falsy values in javascript => 1) Null 2) undefined 3) NaN 4) '' 5) 0 6) false
+
+        const args = [null, undefined, NaN, '', 0, false];
+        args.forEach(a => {
+            expect(() => { registerUser(a as unknown as string) }).toThrow();
+        })
+    });
+
+    it('should return a user object if valid username is passed', () => {
+        const result = registerUser('Shubham');
+        expect(result).toMatchObject({ userName: 'Shubham' });
+        expect(result.id).toBeGreaterThan(0)
+    })
+});
+
+
