844ead458d6a7f3ba0313a058220078bd846b0cd
Shubham Mankar <mankarshubham81@gmail.com>
2024-07-25 17:28:51 +0530

    teat of number and string inout and output

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -1,23 +1,33 @@
-import { absolute } from '../sample';
+import { absolute, greet } from '../sample';
 
 
-test('should first test', () => {
-    console.log("sample first test log");
+describe('absolute', () => {
+    it('should first test', () => {
+        console.log("sample first test log");
+    })
+
+
+    it('should return a positive number if inout is positive', () => {
+        const result = absolute(1);
+        expect(result).toBe(1);
+    });
+
+    it('should return a negative number if input is negative', () => {
+        const result = absolute(-1);
+        expect(result).toBe(1);
+    });
+
+    it('should return a -0 number if input is 0', () => {
+        const result = absolute(0);
+        expect(result).toBe(0);
+    });
+});
+
+describe('greet', () => {
+    it('should return greeting message', () => {
+        const result = greet('Shubham');
+        expect(result).toMatch(/Shubham/);
+        expect(result).toContain('Shubham');
+
+    });
 })
-
-
-test('absoulte - it should return a positive number if inout is positive', () => {
-    const result = absolute(1);
-    expect(result).toBe(1);
-});
-
-test('absoulte - it should return a positive number if input is negative', () => {
-    const result = absolute(-1);
-    expect(result).toBe(1);
-});
-
-test('absoulte - it should return a positive number if input is 0', () => {
-    const result = absolute(0);
-    expect(result).toBe(-0);
-});
-
