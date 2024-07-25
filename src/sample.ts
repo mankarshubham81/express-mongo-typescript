e9c80784280192445b04a4646a41a2e3a1301c7b
Shubham Mankar <mankarshubham81@gmail.com>
2024-07-25 17:29:10 +0530

    add greet function

--- src/sample.ts
+++ src/sample.ts
@@ -1,3 +1,8 @@
 export const absolute = (number: number): number => {
-    return number > 0 ? number : -number;
+    return number >= 0 ? number : -number;
+};
+
+
+export const greet = (name: string): string => {
+    return 'Welcome ' + name;
 };
